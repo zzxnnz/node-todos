efc003d1ec955833efc230e2e44a3853fadf0de2
Ruslan J <leonemsolis@gmail.com>
2018-01-01 18:47:23 +0900

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,16 +1,45 @@
-const jwt = require("jsonwebtoken");
+const b = require("bcryptjs");
 
-var data = {
-    id: 10
-};
+var password = "180rus!";
 
-var token = jwt.sign(data, "abc");
+b.genSalt(10, (err, salt) => {
+    b.hash(password, salt, (err, hash) => {
+        console.log(hash);
+    });
+});
 
-console.log(token);
 
-var decoded = jwt.verify(token, "a");
+var hashedPassword = "$2a$10$9j7KjCPhqaGu87M6GMexQ.voHFnh8Q9QPYA9OxoN4FrEpV7qsy1w.";
 
-console.log(decoded);
+b.compare(password, hashedPassword, (err, result) => {
+    console.log(result);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const jwt = require("jsonwebtoken");
+
+// var data = {
+//     id: 10
+// };
+
+// var token = jwt.sign(data, "abc");
+
+// console.log(token);
+
+// var decoded = jwt.verify(token, "a");
+
+// console.log(decoded);
 
 
 
